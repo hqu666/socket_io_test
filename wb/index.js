c78a0a9246a62efadd0326bf4ab52a5003b16464
CORESOFT\hkuwayama <hkuwayama@coresoft-net.co.jp>
2018-05-17 15:42:39 +0900

    02;XAMPPに移動

--- wb/index.js
+++ wb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3080;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,4 +14,6 @@ function onConnection(socket){
 
 io.on('connection', onConnection);
 
+
+//http.listen(port, () => console.log('EC2>>> ec2-52-197-173-40.ap-northeast-1.compute.amazonaws.com:' + port));
 http.listen(port, () => console.log('web>>> http://127.0.0.1:' + port));
