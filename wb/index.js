c6a8e0dfbc0dde7269c45ac1ba227e688f72e0f4
CORESOFT\hkuwayama <hkuwayama@coresoft-net.co.jp>
2018-05-18 19:04:00 +0900

    06

--- wb/index.js
+++ wb/index.js
@@ -5,15 +5,28 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3080;
 
+
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-  socket.on('allclear', (data) => socket.broadcast.emit('allclear', data));				//�ǋL
+  socket.on('allclear', (data) => socket.broadcast.emit('allclear', data));				//追記
 }
 
 io.on('connection', onConnection);
+//urlの取得
+  // var urlReq = require('url');
 
+  // var requrl = url.format({
+  //     protocol: req.protocol,
+  //     host: req.get('host'),
+  //     pathname: req.originalUrl,
+  // });
+// var urlobj = url.parse(req.originalUrl);
+// urlobj.protocol = req.protocol;
+// urlobj.host = req.get('host');
+// var requrl = url.format(urlobj);
+// var body = req.protocol + '://' + req.headers.host + req.url;
 
 //http.listen(port, () => console.log('EC2>>> ec2-52-197-173-40.ap-northeast-1.compute.amazonaws.com:' + port));
-http.listen(port, () => console.log('web>>> http://127.0.0.1:' + port));
+http.listen(port, () => console.log('web>>> '+  "http://127.0.0.1" +':' + port));
